520d15ddece07214959eeea9acb5a86ac3a9536c
Mews <ar754456@gmail.com>
2024-02-13 22:31:13 +0000

    Added docstrings

--- src/pyrobloxbot/__init__.py
+++ src/pyrobloxbot/__init__.py
@@ -10,6 +10,10 @@ from .literals import KEYBOARD_KEYS, WALK_DIRECTIONS
 UI_NAV_ENABLED = False
 
 def require_focus(fn):
+    """A decorator that ensures the roblox window is in focus before running the decorated function. This is used by all pyrobloxbot functions
+
+    :raises NoRobloxWindowException: Raised when can't find a roblox window to focus
+    """
     #Fast check to see if roblox window is already focused
     if GetWindowText(GetForegroundWindow()) == "Roblox":
         return fn
@@ -39,15 +43,35 @@ def require_focus(fn):
 
 @require_focus
 def keyboard_action(action:KEYBOARD_KEYS.VALUES):
+    """Presses a keyboard key
+
+    :param action: The key to be pressed
+    :type action: KEYBOARD_KEYS
+    """
     dinput.press(action)
 
 @require_focus
-def hold_keyboard_action(action:str, duration:float):
+def hold_keyboard_action(action:KEYBOARD_KEYS.VALUES, duration:float):
+    """Holds a keyboard key for a given time
+
+    :param action: The key to be held
+    :type action: KEYBOARD_KEYS
+    :param duration: How long to hold for, in seconds
+    :type duration: float
+    """
     dinput.keyDown(action)
     wait(duration)
     dinput.keyUp(action)
 
 def walk(direction:WALK_DIRECTIONS.VALUES, duration:float):
+    """Walks in a direction for a given time
+
+    :param direction: The direction to walk in
+    :type direction: WALK_DIRECTIONS
+    :param duration: How long to walk for, in seconds
+    :type duration: float
+    :raises InvalidWalkDirectionException: Raised when given direction isn't in literals.WALK_DIRECTIONS.VALUES
+    """
     direction = direction.lower().strip()
 
     forwardDirections = ["f", "fw", "forward", "forwards"]
@@ -77,54 +101,103 @@ def walk(direction:WALK_DIRECTIONS.VALUES, duration:float):
 
 @require_focus
 def walk_forward(duration:float):
+    """Walks forward for a given time
+
+    :param duration: How long to walk for, in seconds
+    :type duration: float
+    """
     dinput.keyDown("w")
     wait(duration)
     dinput.keyUp("w")
 
 @require_focus
 def walk_left(duration:float):
+    """Walks left for a given time
+
+    :param duration: How long to walk for, in seconds
+    :type duration: float
+    """
     dinput.keyDown("a")
     wait(duration)
     dinput.keyUp("a")
 
 @require_focus
 def walk_right(duration:float):
+    """Walks right for a given time
+
+    :param duration: How long to walk for, in seconds
+    :type duration: float
+    """
     dinput.keyDown("d")
     wait(duration)
     dinput.keyUp("d")
 
 @require_focus
 def walk_back(duration:float):
+    """Walks back for a given time
+
+    :param duration: How long to walk for, in seconds
+    :type duration: float
+    """
     dinput.keyDown("s")
     wait(duration)
     dinput.keyUp("s")
 
 @require_focus
 def jump(number_of_jumps:int=1, delay:float=0):
+    """Jumps for a given number of times
+
+    :param number_of_jumps: How many times to jump, defaults to 1
+    :type number_of_jumps: int
+    :param delay: How much time between jumps, in seconds, defaults to 0
+    :type delay: float
+    """
     for i in range(number_of_jumps):
         dinput.press("space")
         wait(delay)
 
 @require_focus
 def jump_continuous(duration:float):
+    """Holds jump for a given time
+
+    :param duration: How long to hold jump for, in seconds
+    :type duration: float
+    """
     dinput.keyDown("space")
     wait(duration)
     dinput.keyUp("space")
 
 @require_focus
 def reset_player(interval:float=0.5):
+    """Resets player character
+
+    :param interval: How long between each keyboard input, in seconds, defaults to 0.5
+    :type interval: float
+    """
     dinput.press(("esc", "r", "enter"), interval=interval)
 
 @require_focus
 def leave_game(interval:float=0.5):
+    """Leaves the current game
+
+    :param interval: How long between each keyboard input, in seconds, defaults to 0.5
+    :type interval: float
+    """
     dinput.press(("esc", "l", "enter"), interval=interval)
 
 @require_focus
 def toggle_shift_lock():
+    """Toggles shift lock (Shift lock switch must be enabled in roblox settings)
+    """
     dinput.press("shift")
 
 @require_focus
 def chat(message:str):
+    """Sends a message in chat
+
+    :param message: The message to send
+    :type message: str
+    """
     #Open chat
     dinput.keyDown("shift")
     dinput.keyDown("7")
@@ -149,12 +222,16 @@ def chat(message:str):
 
 @require_focus
 def toggle_ui_navigation():
+    """Toggles ui navigation mode. This is called by all ui navigation functions if ui navigation mode is disabled
+    """
     global UI_NAV_ENABLED
     UI_NAV_ENABLED = not UI_NAV_ENABLED
     dinput.press('`')
 
 @require_focus
 def ui_navigate_up():
+    """Navigate up in ui elements
+    """
     if not UI_NAV_ENABLED:
         toggle_ui_navigation()
 
@@ -162,6 +239,8 @@ def ui_navigate_up():
 
 @require_focus
 def ui_navigate_left():
+    """Navigate left in ui elements
+    """
     if not UI_NAV_ENABLED:
         toggle_ui_navigation()
 
@@ -169,6 +248,8 @@ def ui_navigate_left():
 
 @require_focus
 def ui_navigate_right():
+    """Navigate right in ui elements
+    """
     if not UI_NAV_ENABLED:
         toggle_ui_navigation()
 
@@ -176,6 +257,8 @@ def ui_navigate_right():
 
 @require_focus
 def ui_navigate_down():
+    """Navigate down in ui elements
+    """
     if not UI_NAV_ENABLED:
         toggle_ui_navigation()
 
@@ -183,6 +266,8 @@ def ui_navigate_down():
 
 @require_focus
 def ui_click():
+    """Click on currently selected ui element
+    """
     if not UI_NAV_ENABLED:
         toggle_ui_navigation()
 
@@ -190,6 +275,12 @@ def ui_click():
 
 @require_focus
 def equip_slot(slot:int):
+    """Equip a given item slot
+
+    :param slot: The item slot to equip
+    :type slot: int
+    :raises InvalidSlotNumberException: Raised when slot isn't between 0 and 9
+    """
     if slot < 0 or slot > 9:
         raise InvalidSlotNumberException("Slots should be between 0 and 9")
 
--- src/pyrobloxbot/exceptions.py
+++ src/pyrobloxbot/exceptions.py
@@ -1,8 +1,14 @@
 class NoRobloxWindowException(Exception):
+    """Raised when a function can't find a roblox window to focus
+    """
     pass
 
 class InvalidSlotNumberException(Exception):
+    """Raised by equip_slot when slot isn't between 0 and 9
+    """
     pass
 
 class InvalidWalkDirectionException(Exception):
+    """Raised by walk when given a direction that isn't in literals.WALK_DIRECTIONS.VALUES
+    """
     pass
