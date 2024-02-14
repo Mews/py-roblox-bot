6cde9d053775ec1f06148a68789b334f41953dd0
Mews <ar754456@gmail.com>
2024-02-14 09:42:42 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyrobloxbot',
+      version='1.0.5',
+      description="My sample package",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['pyrobloxbot'],
+      zip_safe=False,
+      install_requires=[],
+      )
