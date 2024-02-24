ef845c04ca83c45314c651264c2c86af253c9ad9
Mews <ar754456@gmail.com>
2024-02-24 16:04:14 +0000

    Mock pynpuy and pyautogui

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ project = 'pyrobloxbot'
 copyright = '2024, Mews'
 author = 'Mews'
 release = '1.0.5'
-autodoc_mock_imports = ["pydirectinput", "win32gui", "pygetwindow", "keyboard"]
+autodoc_mock_imports = ["pydirectinput", "win32gui", "pygetwindow", "keyboard", "pynput", "pyautogui"]
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
